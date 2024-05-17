46e9204ee541296c5671fe32c47a47f82efada93
Pranay Sherkar <116248469+Pranaysherkar@users.noreply.github.com>
2024-05-17 13:33:15 +0530

    second commit

--- src/utils/Context.jsx
+++ src/utils/Context.jsx
@@ -4,19 +4,21 @@ import React, { createContext, useEffect, useState } from "react";
 export const ProductContext = createContext();
 
 function Context(props) {
-  const [products, setproducts] = useState(JSON.parse(localStorage.getItem("products")) || null);
+  const [products, setproducts] = useState(null);
+  // const [products, setproducts] = useState(JSON.parse(localStorage.getItem("products")) || null ); //after loading data store in local storage
 
-  // const getProducts = async()=>{
-  //   try{
-  //     const {data}=await axios.get("/products");
-  //     setproducts(data)
-  //   }catch(err){
-  //     console.log(err);
-  //   }
-  // };
-  // useEffect(()=>{
-  //   getProducts();
-  // },[])
+  //getting data from api
+  const getProducts = async()=>{
+    try{
+      const {data}=await axios.get("/products");
+      setproducts(data)
+    }catch(err){
+      console.log(err);
+    }
+  };
+  useEffect(()=>{
+    getProducts();
+  },[])
   console.log(products);
   return (
     <ProductContext.Provider value={[products, setproducts]}>
